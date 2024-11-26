947ad71dbf03eca249988c9e4d130216aa97ded3
Shiran Levi <shiranle07@gmail.com>
2024-11-26 17:52:35 +0200

    App logo addition

--- my_health_tracker/src/loginPage/welcome/Welcome.js
+++ my_health_tracker/src/loginPage/welcome/Welcome.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './Welcome.css';
+import logo from './logo.png';  // Import SVG as a React component
 
-function Welcome(){
-    return (
-        <div className="col-12 col-md-6 col-lg-4">
-        <h2 className="text-center mb-4 text-maroon icon">
-          Welcome to
-        </h2>
-        <h2 className="text-center mb-4 text-maroon icon app-name">
-          <i className="fas fa-heartbeat me-2"></i>My Health Tracker
-        </h2>
-        <div className="text-center mb-4">
-          <p className="text-maroon">Track your health, manage your fitness goals, and stay on top of your well-being.</p>
-        </div>
+function Welcome() {
+  return (
+    <div className="col-12 col-md-6 col-lg-4 text-center">
+      <img className="App-logo" alt="logo" src={logo} />
+      <h2 className="text-center mb-4 text-maroon icon">
+        Welcome to  
+      </h2>
+      <h2 className="text-center mb-4 text-maroon icon app-name">
+        <i className="fas fa-heartbeat me-2"></i>My Health Tracker
+      </h2>
+      <div className="text-center mb-4">
+        <p className="text-maroon">Track your health, manage your fitness goals, and stay on top of your well-being.</p>
       </div>
-    );
+    </div>
+  );
 }
+
 export default Welcome;
